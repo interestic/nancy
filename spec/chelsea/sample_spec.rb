ab07cc1f5d7edaf35d3ace03668df7abc27bc679
rayfields <raysbacker@gmail.com>
2016-06-04 02:50:09 +0900

    Modifying spec

--- spec/chelsea/sample_spec.rb
+++ spec/chelsea/sample_spec.rb
@@ -4,21 +4,7 @@ describe package('httpd'), :if => os[:family] == 'redhat' do
   it { should be_installed }
 end
 
-describe package('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
-
 describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
   it { should be_enabled }
   it { should be_running }
 end
